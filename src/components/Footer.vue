7689b6a29a626c6822b85ed9bc1f8c99625ef7b3
Midstackdev <midstackdev@gmail.com>
2020-08-02 18:33:08 +0000

    App structure layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer id="custom-footer">
+        <span class="mr-4">&copy; 20018 Midstackdev</span>
+        <span class="mr-4">Private</span>
+        <span>Terms of service</span>
+    </footer>
+</template>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="jumbotron custom-bg-dark">
+      <h1 class="display-4">MEVN Stack Task Manager</h1>
+      <p class="lead">This is a simple task manager application, built with MongoDB, ExpressJS, VueJS, and NodeJS.</p>
+      <hr class="my-4">
+      <p>Click here to begin managing task for users.</p>
+      <a class="btn btn-success btn-lg" href="#" role="button">View Tasks</a>
+    </div>
+</template>
