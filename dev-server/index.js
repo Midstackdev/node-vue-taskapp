fd4b8e0a79926df7e91fb226571b4d26e0b8fc77
Midstackdev <midstackdev@gmail.com>
2020-08-03 00:16:32 +0000

    Setting server env

--- dev-server/index.js
+++ dev-server/index.js
@@ -4,13 +4,21 @@ const port = 5100
 const hostname = '127.0.0.1';
 
 import { registerRoutes } from './routes'
+import { setEnvironment} from './config/env'
 
+setEnvironment(app)
 registerRoutes(app)
 
 app.get('/', (req, res) => {
-  res.send('Hello World people!')
+    if(process.env.NODE_ENV !== 'production') {
+        return res.send(
+            'Running server in development mode.'
+        )
+    } else {
+        return res.sendFile('index.html', { root: __dirname + '/../dist/'})
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Mevn Stack app listening at http://${hostname}:${port}`)
+  console.log(`Mevn Stack app listening at http://${hostname}:${port} in ${process.env.NODE_ENV}`)
 })
--- prod-server/index.js
+++ prod-server/index.js
@@ -6,18 +6,25 @@ var _express2 = _interopRequireDefault(_express);
 
 var _routes = require('./routes');
 
+var _env = require('./config/env');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 var port = 5100;
 var hostname = '127.0.0.1';
 
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 
 app.get('/', function (req, res) {
-  res.send('Hello World people!');
+    if (process.env.NODE_ENV !== 'production') {
+        return res.send('Running server in development mode.');
+    } else {
+        return res.sendFile('index.html', { root: __dirname + '/../dist/' });
+    }
 });
 
 app.listen(port, function () {
-  console.log('Mevn Stack app listening at http://' + hostname + ':' + port);
+    console.log('Mevn Stack app listening at http://' + hostname + ':' + port + ' in ' + process.env.NODE_ENV);
 });
